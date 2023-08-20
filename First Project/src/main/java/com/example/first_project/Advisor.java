1b9e0fb9f9c072224fff76c144281655dd6f7c5b
Vandena2 <Scienceinstein1@gmail.com>
2023-08-20 22:50:48 +0300

    This is my finalized project.

--- First Project/src/main/java/com/example/first_project/Advisor.java
+++ First Project/src/main/java/com/example/first_project/Advisor.java
@@ -1,1 +1,51 @@
+package com.example.first_project;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+@Entity
+public class Advisor {
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private Integer phone;
+
+    protected Advisor(){
+
+    }
+
+    public Advisor(String firstName, String lastName, Integer phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+}
